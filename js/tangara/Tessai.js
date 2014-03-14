b2f52e0aa3ca9d7e0f3eb5cfa89b8b5db2b04b67
Dihia-A <addad_dihia@hotmail.fr>
2014-03-14 10:02:41 +0100

    Create Tessai.js

---
+++ js/tangara/Tessai.js
@@ -0,0 +1,9 @@
+define(['jquery', 'split-pane','TCanvas', 'TEnvironment', 'TEditor', 'TLog','Tessai'], function($, SplitPane, TCanvas, TEnvironment, TEditor, TLog,Tessai) {
+    function TAffiche() {
+
+      console.log("Bonjour");
+
+
+    }
+    
+}
